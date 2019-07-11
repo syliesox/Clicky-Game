e2c45aa9e5fa3ae142e6d7c370952e9362b25ad5
syliesox <syliesox@gmail.com>
2019-07-11 19:51:13 -0400

    fixed header wording

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -3,7 +3,8 @@ import "./style.css";
 
 const Body = (props) => (
     <div class="wrapper">
-        
+        <h3>Click on a Looney Tunes character below to earn points!</h3>
+        <h3>But don't click on the same image more than once!</h3>
     </div>
 );
 
