453ec3bcd907616050c95e8c056b4d763dd7707e
syliesox <syliesox@gmail.com>
2019-07-15 17:13:35 -0400

    Updates

---
+++ precache-manifest.9facd9f5032dc25ee67802580eee4647.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08bca60eb067e619aa8b17496f36afb",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "e7e90a97678219ab1209",
+    "url": "/Clicky-Game/static/css/main.c76865b4.chunk.css"
+  },
+  {
+    "revision": "afab084484d13aec2faa",
+    "url": "/Clicky-Game/static/js/2.e8f3f846.chunk.js"
+  },
+  {
+    "revision": "e7e90a97678219ab1209",
+    "url": "/Clicky-Game/static/js/main.5d036d0d.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "96d2904f5255899e14373125326b5135",
+    "url": "/Clicky-Game/static/media/bugs_bunny.96d2904f.gif"
+  },
+  {
+    "revision": "9027d84d3b22608b2974f20758a10df4",
+    "url": "/Clicky-Game/static/media/coyote.9027d84d.gif"
+  },
+  {
+    "revision": "f71e8fa6d42ae2c12a38acc9e989196f",
+    "url": "/Clicky-Game/static/media/daffy_duck.f71e8fa6.gif"
+  },
+  {
+    "revision": "a817b3350d0638021afa57a784bd2b51",
+    "url": "/Clicky-Game/static/media/elmer_fudd.a817b335.gif"
+  },
+  {
+    "revision": "70066f25d3315ac60b3765ee69a39883",
+    "url": "/Clicky-Game/static/media/foghorn.70066f25.gif"
+  },
+  {
+    "revision": "45baa6a77d68bf86fc3e4a19da40d34d",
+    "url": "/Clicky-Game/static/media/frog.45baa6a7.gif"
+  },
+  {
+    "revision": "d2b6dbabc5660c983ebcc247439b6f40",
+    "url": "/Clicky-Game/static/media/martian.d2b6dbab.gif"
+  },
+  {
+    "revision": "451658dca74a37b0ee780c359aae5814",
+    "url": "/Clicky-Game/static/media/pepe.451658dc.gif"
+  },
+  {
+    "revision": "f336bf9d11c5198df43b87846426a7b0",
+    "url": "/Clicky-Game/static/media/porky_pig.f336bf9d.gif"
+  },
+  {
+    "revision": "7b4f10720452a2bbfbafac0fcf4cd6c3",
+    "url": "/Clicky-Game/static/media/road_runner.7b4f1072.gif"
+  },
+  {
+    "revision": "2e3135fc2e4f6142a0dad46b01ae6edb",
+    "url": "/Clicky-Game/static/media/sam.2e3135fc.gif"
+  },
+  {
+    "revision": "46f33e1ba97707ae67dc2cb7d68a9bf3",
+    "url": "/Clicky-Game/static/media/speedy.46f33e1b.gif"
+  },
+  {
+    "revision": "9637a2a081d2b0429a0cffe4a7e592e7",
+    "url": "/Clicky-Game/static/media/sylvester.9637a2a0.gif"
+  },
+  {
+    "revision": "7eadd7bc17c0ec954413f3b28fb84d5d",
+    "url": "/Clicky-Game/static/media/taz.7eadd7bc.gif"
+  },
+  {
+    "revision": "2e6b773e6cd0971ed03f3027bb5f37d3",
+    "url": "/Clicky-Game/static/media/tweety.2e6b773e.gif"
+  }
+]);
